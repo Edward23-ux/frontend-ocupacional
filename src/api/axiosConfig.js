d19f9f4886966a6ae0bf21045f089055b730998f
Edward23-ux <edwardcampos46@gmail.com>
2026-07-17 08:39:41 -0500

    debug: add console.log to base url

--- src/api/axiosConfig.js
+++ src/api/axiosConfig.js
@@ -6,6 +6,8 @@ const BASE_URL = import.meta.env.VITE_API_URL
   ? `${import.meta.env.VITE_API_URL}/api/`
   : 'http://localhost:8080/api/'
 
+console.log('--- API BASE URL ---', BASE_URL)
+
 export const api = axios.create({
   baseURL: BASE_URL,
   headers: {
